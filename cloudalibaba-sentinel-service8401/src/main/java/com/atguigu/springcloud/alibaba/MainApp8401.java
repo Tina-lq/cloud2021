164c7eb18258daf15307fb75f02680b558ab49ab
Tina-lq <480384231@qq.com>
2021-05-08 11:42:40 +0800

    加入sentinel做监控

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/MainApp8401.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName MainApp8401
+ * @Author Liucz
+ * @Description Sentinel监控的启动类
+ * @Date 2021/5/7 16:53
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimiController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName FlowLimiController
+ * @Author Liucz
+ * @Description Sentinel测试controller
+ * @Date 2021/5/7 17:00
+ **/
+@RestController
+public class FlowLimiController {
+
+    @GetMapping("/testA")
+    public String testA(){
+        return "-----testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        return "-----testB";
+    }
+
+}
